7f5edee55c0e687bbd963de51bca33b26b74c265
aibashev adilet <adilet.aibashev.64@gmail.com>
2021-12-06 19:30:55 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+
+
+package com.company;
+import  java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner str = new Scanner(System.in);
+        System.out.println("Введите ваше имя");
+        System.out.println( "Здравствуйте "+str.nextLine ());
+    }
+}
